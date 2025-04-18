4358b9b1fea9edb6673d86f351c291113b311636
Matteoiva <matteoivagnes25@gmail.com>
2025-04-18 18:22:38 +0200

    ok

--- src/app/controller/Controller.java
+++ src/app/controller/Controller.java
@@ -30,51 +30,9 @@ public class Controller {
 	public void RegistrazioneUtente(String nome, String email, String psw) {
 		new ThRegistraUtente(nome,email,psw).start();
 	}	
-	@SuppressWarnings("resource")
+
 	private boolean UtenteEsiste(String nome, String email, String psw) {
-		File cartella=new File("UtentiRegistrati/");
-		for(String s: cartella.list()) {
-	        try {
-	            Scanner scanner = new Scanner(new File("UtentiRegistrati/"+s));
-	            
-	            boolean Briga1,Briga2,Briga3;
-	            
-	            String riga1 = scanner.nextLine();
-	            if(riga1.equals(nome))
-	            	Briga1=true;
-	            else
-	            	Briga1=false;
-	            
-	            String riga2 = scanner.nextLine();	
-	            if(riga2.equals(email))
-	            	Briga2=true;
-		        else
-		            Briga2=false;
-		            
-	            
-	            String riga3 = scanner.nextLine();	
-	            if(riga2.equals(psw))
-	            	Briga3=true;
-		        else
-		            Briga3=false;
-		            
-	            if(Briga1&&Briga2&&Briga3) {
-	            	SaveProfile(nome, email, psw);
-	            	return true;
-	            }
-
-	            scanner.close();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-		}
-
-		return false;
-	}
-	
-	//gestione file
-	private void SaveProfile(String nome, String email, String psw) {
-		new ThSaveProfile(nome, email, psw).start();;
+		return model.IsUtenteEsistente(nome, email, psw);
 	}
 	
 	public Boolean coockieLogin() {
@@ -84,24 +42,7 @@ public class Controller {
 	}
 
     public String getElementoFileLogin(Predicate<String> p) {
-        String stringa = "";
-        try {
-            Scanner s = new Scanner(new File("Credenziali/cookieCredenziali.txt"));
-            
-            if (s.hasNextLine()) {
-                String riga1 = s.nextLine();
-                if (!p.test("email")) 
-                    stringa = riga1;
-                else 
-                    if (s.hasNextLine()) 
-                        stringa = s.nextLine();
-                    
-                }
-            s.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return stringa; 
+    	return model.getElementoFileLogin(p);
     }
 	
 	public void logout() {
--- src/app/model/Model.java
+++ src/app/model/Model.java
@@ -2,7 +2,10 @@ package app.model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.function.Predicate;
+
+import app.Thread.ThSaveProfile;
 
 public class Model {
 	private String nomeCartellaPrincipale;
@@ -36,8 +39,71 @@ public class Model {
 				break;
 		return finalString;
 	}
+	@SuppressWarnings("resource")
+	public boolean IsUtenteEsistente(String nome, String email, String psw) {
+		File cartella=new File("UtentiRegistrati/");
+		for(String s: cartella.list()) {
+	        try {
+	            Scanner scanner = new Scanner(new File("UtentiRegistrati/"+s));
+	            
+	            boolean Briga1,Briga2,Briga3;
+	            
+	            String riga1 = scanner.nextLine();
+	            if(riga1.equals(nome))
+	            	Briga1=true;
+	            else
+	            	Briga1=false;
+	            
+	            String riga2 = scanner.nextLine();	
+	            if(riga2.equals(email))
+	            	Briga2=true;
+		        else
+		            Briga2=false;
+		            
+	            
+	            String riga3 = scanner.nextLine();	
+	            if(riga2.equals(psw))
+	            	Briga3=true;
+		        else
+		            Briga3=false;
+		            
+	            if(Briga1&&Briga2&&Briga3) {
+	            	SaveProfile(nome, email, psw);
+	            	return true;
+	            }
+
+	            scanner.close();
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+		}
+
+		return false;
+	}
+	private void SaveProfile(String nome, String email, String psw) {
+		new ThSaveProfile(nome, email, psw).start();;
+	}
 	
-	
+	public String getElementoFileLogin(Predicate<String> p) {
+        String stringa = "";
+        try {
+            Scanner s = new Scanner(new File("Credenziali/cookieCredenziali.txt"));
+            
+            if (s.hasNextLine()) {
+                String riga1 = s.nextLine();
+                if (!p.test("email")) 
+                    stringa = riga1;
+                else 
+                    if (s.hasNextLine()) 
+                        stringa = s.nextLine();
+                    
+                }
+            s.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return stringa; 
+	}
 	
 	
 	
